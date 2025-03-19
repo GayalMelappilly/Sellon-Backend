f51031177d6c8bd53c55d6a6fc07cf46e426c8b2
Gayal Melappilly <gayalsunil@gmail.com>
2025-03-20 00:50:59 +0530

    fix(deploy): firebase config error

--- utils/firebaseAdmin.ts
+++ utils/firebaseAdmin.ts
@@ -1,11 +1,22 @@
 import admin from 'firebase-admin'
 import path from 'path'
+import { configDotenv } from 'dotenv'
+configDotenv()
 
-const serviceAccount = require(path.join(__dirname, '../firebase-config/sellon-5beb3-firebase-adminsdk-fbsvc-e5a6f6c9ec.json'))
-
-if(!admin.apps.length) {
+if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
+        credential: admin.credential.cert({
+            type: process.env.FIREBASE_TYPE as string,
+            project_id: process.env.FIREBASE_PROJECT_ID,
+            private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+            private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            client_email: process.env.FIREBASE_CLIENT_EMAIL,
+            client_id: process.env.FIREBASE_CLIENT_ID,
+            auth_uri: process.env.FIREBASE_AUTH_URI,
+            token_uri: process.env.FIREBASE_TOKEN_URI,
+            auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+            client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+        } as any)
     })
 }
 
